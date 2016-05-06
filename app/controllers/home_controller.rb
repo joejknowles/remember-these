c35c090bb572b9c2a580037d1916894e66250d50
Joseph Knowles <joejknowles@gmail.com>
2016-05-06 19:22:30 +0200

    styled

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,23 +7,23 @@ class HomeController < ApplicationController
     @username = params[:username]
     @all = all_bands
     @last_month = last_month
-    @result = @all - @last_month
+    @results = most_missed
     render :index
   end
 
   def all_bands
-    JSON.parse(open("http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=#{@username}&period=overall&api_key=4b62ea447ec4381e040a1bea57bc2a57&format=json").read)['topartists']['artist'].map {|artist| artist['name'] }
+    JSON.parse(open("http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=#{@username}&period=overall&api_key=4b62ea447ec4381e040a1bea57bc2a57&format=json").read)['topartists']['artist']
   end
 
   def last_month
-    JSON.parse(open("http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=#{@username}&period=1month&api_key=4b62ea447ec4381e040a1bea57bc2a57&format=json").read)['topartists']['artist'].map {|artist| artist['name'] }
-  end
+    JSON.parse(open("http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=#{@username}&period=1month&api_key=4b62ea447ec4381e040a1bea57bc2a57&format=json").read)['topartists']['artist']  end
 
   def last_week
-    JSON.parse(open("http://ws.audioscrobbler.com/2.0/?method=user.getweeklyartistchart&user=#{@username}&api_key=4b62ea447ec4381e040a1bea57bc2a57&format=json").read)['weeklyartistchart']['artist'].map {|artist| artist['name'] }
+    JSON.parse(open("http://ws.audioscrobbler.com/2.0/?method=user.getweeklyartistchart&user=#{@username}&api_key=4b62ea447ec4381e040a1bea57bc2a57&format=json").read)['weeklyartistchart']['artist']
   end
 
   def most_missed
-    @all_bands - @last_month
+    recent_names = @last_month.map { |m_artist| m_artist["name"] }
+    @all.reject { |band| recent_names.include? band["name"]}
   end
 end
